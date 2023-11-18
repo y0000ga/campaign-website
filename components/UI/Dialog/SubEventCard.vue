4327cbcd3cc0e56b8f9e0e8281c63a6272ac5ea5
Yu-Jia Huang <shiusun246@gmail.com>
2023-11-19 04:39:01 +0800

    fix: more event card width

--- components/UI/Dialog/SubEventCard.vue
+++ components/UI/Dialog/SubEventCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card :class="['bg-transparent elevation-0', isDesktop && 'w-50']"
+  <v-card :class="['bg-transparent elevation-0', isDesktop ? '' : 'w-50']"
     :style="{ ...isDesktop && { 'max-width': '244px' } }">
     <img :src="props.imgSrc" class="rounded-lg w-100"
       :style="{ ...isDesktop && { height: '134px' }, aspectRatio: '244 / 134', objectFit: 'cover' }" />
